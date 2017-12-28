0d0943ba212f5d8511af4a35f91ac5c006f2338b
Abbadon771 <james.norcross@att.net>
2017-12-28 14:35:57 -0800

    Add files via upload

---
+++ scripts/followCam.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followCam : MonoBehaviour {
+
+    public Transform target;
+    private Vector3 defaultDist = new Vector3(0.5f, 4f, -15f);
+    private float distanceDamp = 10;
+    private float rotationalDamp = 10f;
+    Transform myT;
+
+    void Awake()
+    {
+        myT = transform;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 toPos = target.position + (target.rotation * defaultDist);
+        Vector3 curPos = Vector3.Lerp(myT.position, toPos, distanceDamp * Time.deltaTime);
+        myT.position = curPos;
+
+        Quaternion toRot = Quaternion.LookRotation(target.position - myT.position, target.up);
+        Quaternion curRot = Quaternion.Slerp(myT.rotation, toRot, rotationalDamp * Time.deltaTime);
+        myT.rotation = curRot;
+    }
+}
---
+++ scripts/playerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+    public float TurningSpeed = 100;
+    Vector3 LookAtPos;
+    Vector3 SmoothedLookAtPos;
+    public float ForwardSpeed = 100;
+    void Update()
+    {
+        //Look At the Mouse
+        LookAtPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 100f));
+        SmoothedLookAtPos = Vector3.Lerp(SmoothedLookAtPos, LookAtPos, Time.deltaTime * 5);
+        transform.LookAt(SmoothedLookAtPos);
+        //Move forward (you could use a Rigidbody)
+        transform.position += transform.forward * Time.deltaTime * ForwardSpeed;
+    }
+}
